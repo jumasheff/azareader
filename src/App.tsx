2b6f93c7374d7ff6b5bb259601df18609af1d669
murat <jumasheff@gmail.com>
2023-05-27 22:13:39 +0600

    Add event listeners to all page links

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,82 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { CSSProperties, useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const superInfo = "https://azattyk.org";
+  const [pageSource, setPageSource] = useState("<p>Greetings!</p>");
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+  async function sendRequestToBackend(URL: string) {
+    const msg: string = await invoke("greet", { name: URL });
+    setPageSource(msg);
   }
 
+  useEffect(() => {
+    sendRequestToBackend(superInfo);
+
+    function handleMessage(event: MessageEvent) {
+      if (event.data.type === "linkClicked") {
+        let urlObj = new URL(event.data.href);
+        if (urlObj.origin.includes("localhost")) {
+          urlObj = new URL(superInfo + urlObj.pathname);
+        }
+        sendRequestToBackend(urlObj.href);
+      }
+    }
+
+    window.addEventListener("message", handleMessage);
+    return () => window.removeEventListener("message", handleMessage);
+  }, []);
+
+  const style = {
+    margin: "0px",
+    padding: "0px",
+    overflow: "hidden",
+  };
+
+  const iFrameStyle: CSSProperties = {
+    overflow: "hidden",
+    overflowX: "hidden",
+    overflowY: "hidden",
+    height: "100%",
+    width: "100%",
+    position: "absolute",
+    top: "0px",
+    left: "0px",
+    right: "0px",
+    bottom: "0px",
+  };
+
+  const script = `
+    <script>
+      const links = document.querySelectorAll("a");
+      links.forEach((link) => {
+        link.addEventListener("click", (event) => {
+          event.preventDefault();
+          window.parent.postMessage(
+            {
+              type: "linkClicked",
+              href: link.href,
+            },
+            "*"
+          );
+        });
+      });
+    </script>
+  `;
+
+  const srcDoc = pageSource.replace("</body>", script + "</body>");
+
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
-    </div>
+    <body style={style}>
+      <iframe
+        srcDoc={srcDoc}
+        style={iFrameStyle}
+        title="greet"
+        width="100%"
+        height="100vh"
+      />
+    </body>
   );
 }
 
