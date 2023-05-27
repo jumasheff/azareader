98c2f36406f58a900315e9c8cbe5af25792d0876
murat <jumasheff@gmail.com>
2023-05-27 22:12:58 +0600

    Test with proxy

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,31 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+
+use std::{time::Duration, io::Read};
+
+use reqwest::{Proxy};
+
+fn fetch_url_content(url: &str) -> String {
+    let raw_proxy = format!("127.0.0.1:18080");
+    let proxy = Proxy::all(&raw_proxy).unwrap();
+    let client = reqwest::blocking::Client::builder()
+            .timeout(Duration::from_secs(10))
+            .proxy(proxy)
+            .build()
+            .unwrap();
+    let  mut response = client.get(url).send().unwrap();
+    let mut buf = String::new();
+    response.read_to_string(&mut buf).expect("Failed to read response");
+    buf
+}
+
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    let content = fetch_url_content(&name);
+    content
 }
 
 fn main() {
